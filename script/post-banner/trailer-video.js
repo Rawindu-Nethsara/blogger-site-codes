cd7fb268bd9fc69415171b2abb1bf19c0895346a
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-08-18 00:50:04 -0700

    Create trailer-video.js

---
+++ script/post-banner/trailer-video.js
@@ -0,0 +1,12 @@
+  ////////// TRAILER VIDEO
+const popup = document.getElementById("trailerPopup");
+const frame = document.getElementById("trailerFrame");
+
+function openPopup() {
+  popup.style.display = "flex";
+}
+function closePopup() {
+  popup.style.display = "none";
+  // Stop video by resetting src
+  frame.src = frame.src;
+}
