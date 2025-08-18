9c027ef27153f199e05c78ed9d096617bcf39b61
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-08-18 01:24:54 -0700

    Delete script/post-banner/trailer-video.js

--- script/post-banner/trailer-video.js
+++
@@ -1,6 +0,0 @@
-/////trailer-video.
-const popup = document.getElementById("trailerPopup");
-const frame = document.getElementById("trailerFrame");
-function openPopup(){popup.style.display="flex";}
-function closePopup(){popup.style.display="none"; frame.src=frame.src;}
-popup.onclick = closePopup;
