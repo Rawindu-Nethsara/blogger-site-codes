98e77740c77d907be8d9494adeb501444b774a33
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-08-18 01:22:35 -0700

    Update trailer-video.js

--- script/post-banner/trailer-video.js
+++ script/post-banner/trailer-video.js
@@ -1,15 +1,6 @@
-/////trailer-video
+/////trailer-video.
 const popup = document.getElementById("trailerPopup");
 const frame = document.getElementById("trailerFrame");
-
-function openPopup() {
-  popup.style.display = "flex";
-}
-
-function closePopup() {
-  popup.style.display = "none";
-  // Stop video
-  frame.src = frame.src;
-}
-
+function openPopup(){popup.style.display="flex";}
+function closePopup(){popup.style.display="none"; frame.src=frame.src;}
 popup.onclick = closePopup;
