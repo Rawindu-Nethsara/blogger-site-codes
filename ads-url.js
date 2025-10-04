baa10aebb02f797a713bc80c8b6bbe3ba36c1506
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-10-04 11:21:19 +0530

    Create ads-url.js

---
+++ ads-url.js
@@ -0,0 +1,7 @@
+// ads.js
+const ADS_URLS = [
+  "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+  "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+  "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4"
+];
+export default ADS_URLS;
