8056521d514e9424ac23d09fd9642b00ae70e92c
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-10-04 23:17:12 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,5 +1,7 @@
 window.ADS_URLS = [
   "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
   "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+  "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+  "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4",
   "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4"
 ];
