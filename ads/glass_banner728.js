815d0a08e2566c017953e10a4518d8d95f5e58c3
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-10-11 18:29:25 +0530

    Update glass_banner728.js

--- ads/glass_banner728.js
+++ ads/glass_banner728.js
@@ -1,10 +1,12 @@
+document.write(`
 <script type="text/javascript">
-	atOptions = {
-		'key' : '7f8292d63d798b97c503a0b40480244f',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
+  atOptions = {
+    'key': '7f8292d63d798b97c503a0b40480244f',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+  };
 </script>
 <script type="text/javascript" src="//www.highperformanceformat.com/7f8292d63d798b97c503a0b40480244f/invoke.js"></script>
+`);
