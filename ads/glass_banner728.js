a87402ee4a5bd7cc1d17183395d3e2b03290eb50
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-10-11 18:32:11 +0530

    Update glass_banner728.js

--- ads/glass_banner728.js
+++ ads/glass_banner728.js
@@ -1,12 +1,29 @@
-document.write(`
-<script type="text/javascript">
-  atOptions = {
-    'key': '7f8292d63d798b97c503a0b40480244f',
-    'format': 'iframe',
-    'height': 90,
-    'width': 728,
-    'params': {}
-  };
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/7f8292d63d798b97c503a0b40480244f/invoke.js"></script>
-`);
+(function() {
+  var container = document.getElementById('ad-container');
+  if (!container) return;
+
+  // Clear "Ad loading..." message
+  container.innerHTML = '';
+
+  // Create Adsterra main script dynamically
+  var adScript = document.createElement('script');
+  adScript.type = 'text/javascript';
+  adScript.innerHTML = `
+    atOptions = {
+      'key': '7f8292d63d798b97c503a0b40480244f',
+      'format': 'iframe',
+      'height': 90,
+      'width': 728,
+      'params': {}
+    };
+  `;
+
+  // Create Adsterra invoke.js loader
+  var invokeScript = document.createElement('script');
+  invokeScript.type = 'text/javascript';
+  invokeScript.src = '//www.highperformanceformat.com/7f8292d63d798b97c503a0b40480244f/invoke.js';
+
+  // Inject both into the container
+  container.appendChild(adScript);
+  container.appendChild(invokeScript);
+})();
