443bf041a91de26220445e532c27b8323dfbf88d
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-10-11 18:17:34 +0530

    Create glass_banner728.js

---
+++ ads/glass_banner728.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '7f8292d63d798b97c503a0b40480244f',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/7f8292d63d798b97c503a0b40480244f/invoke.js"></script>
